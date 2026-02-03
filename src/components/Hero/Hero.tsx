fecf64098a60c4cac82eef578210d030553fba79
Samiul Islam Ramim <123535324+SamiulIslamRamim@users.noreply.github.com>
2026-02-03 17:04:28 +0600

    -built navbar -started content section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='content'>
+
+      <div className='subhead'>
+        One payments platform built for every business
+        </div>
+
+      <div className='head'>
+        One Platform 
+for Every Business
+</div>
+      
+      <div className='inp'>
+        <input type="text" />
+        <input type="button" value="Join Beta" />
+      </div>
+    
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import logo from "../../assets/images/Vector.svg";
+const Navbar = () => {
+  return (
+    <div className="nav">
+
+      <div className="veli">
+        <div className="logo">
+            <img src={logo}></img>
+        </div>
+        <div className="velLog">Velio</div>
+      </div>
+
+      <div className="opt">
+        <div>Home</div>
+        <div>About</div>
+        <div>Platform</div>
+        <div>Features</div>
+        <div>FAQ</div>
+      </div>
+
+      <button className="strt">
+        <p className="bttext">Get started (free)</p>
+      </button>
+
+    </div>
+  );
+};
+
+export default Navbar;
